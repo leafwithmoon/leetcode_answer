587c22eec9594a4c04b48d7a6b8360204a45bc0a
Hisky <leafwithmoon@gmail.com>
2023-10-11 00:20:16 +0800

    to be finisned

---
+++ longest-no-repeating-substring/longest-substring.go
@@ -0,0 +1,10 @@
+package longestNoRepeatingSubstring
+
+func lengthOfLongestSubstring(s string) int {
+
+	// how to find no repeating substring?
+
+	// 1. how to find substring
+	// 2. no repeating
+	return 0
+}
